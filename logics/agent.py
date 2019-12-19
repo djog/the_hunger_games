5d7311785c7a561bb82aa74227fe7fe4d3aff223
RafayelGardishyan <rgardishyan@gmail.com>
2019-12-19 21:44:33 +0100

    Added agent

--- logics/agent.py
+++ logics/agent.py
@@ -1,8 +1,37 @@
 from pygame.math import Vector2
 
 
+class Action:
+
+    def __init__(self, type, args):
+        self.type = type
+        self.args = args
+
+
 class Agent(object):
+
     def __init__(self):
         self.lifetime = 0
         self.location = Vector2()
         self.location.x, self.location.y = 0, 0
+
+        self.action_queue: list = []  # Of type Action
+
+        self.actions = {
+            "MOV": self.move
+        }
+
+    def move(self, offset: tuple):
+        self.location.x, self.location.y = offset
+
+    def action(self, atype: str, arguments: tuple):
+        self.action_queue.append(
+            Action(atype, arguments)
+        )
+
+    def update(self):
+        for action in self.action_queue:
+            try:
+                self.actions[action.type](action.args)
+            except KeyError:
+                print("No such action: {}".format(action.type))
--- tests/test_runner.py
+++ tests/test_runner.py
@@ -1,4 +1,5 @@
 # author: Rafayel Gardishyan
+from logics.agent import Agent
 
 
 def function(a, b):
@@ -7,3 +8,10 @@ def function(a, b):
 
 def test_test():
     assert function(3, 5) == 13
+
+
+def test_agent():
+    a = Agent()
+    a.action("MOV", (5, 2))
+    a.update()
+    assert a.location.x, a.location.y == (5, 2)
