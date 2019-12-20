e8f5e29b310f851f4df3d992f9f54bceaf4d7bc7
Rob Krüger <4958@praedinius.nl>
2019-12-20 19:36:10 +0100

    drawing works again

--- graphics/pygame_manager.py
+++ graphics/pygame_manager.py
@@ -5,11 +5,12 @@ from logics.world import World
 
 class Graphics(object):
 
-    def __init__(self, window_size):
-        self.w = World()
+    def __init__(self, window_size, world_size):
+        self.w = World((world_size, world_size))
         pygame.init()
         self.screen = pygame.display.set_mode(window_size)
         self.Running = True
+        self.gridsize = world_size
 
     def parse_events(self):
         for event in pygame.event.get():
@@ -19,14 +20,19 @@ class Graphics(object):
     def draw(self):
         self.screen.fill((255, 255, 255))
 
-        cell_size = 0  # Calculate cell size based on the world size and window size
+        # Calculate cell size based on the world size and window size
+        cell_size = int(((pygame.display.Info().current_w - 30) / self.gridsize) + 0.5)
+        print(cell_size)
+        offset = (pygame.display.Info().current_w - (self.gridsize * cell_size)) / 2
 
         # draw
+        for x in range(self.gridsize * self.gridsize):
+            pygame.draw.rect(self.screen, [0, 0, 0], [(x % self.gridsize) * cell_size + offset, int(x / self.gridsize) * cell_size + offset, cell_size, cell_size], 1)
 
         pygame.display.flip()
 
     @staticmethod
-    def close(self):
+    def close():
         pygame.quit()
 
     # for x in range(grid_size * grid_size):
--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from graphics.pygame_manager import Graphics
 if __name__ == "__main__":
     print("Hello SNEK")
 
-    p = Graphics((600, 600))
+    p = Graphics((600, 600), 15)
 
     while p.Running:
         p.parse_events()
         p.draw()
-        p.close()
+
+    p.close()
