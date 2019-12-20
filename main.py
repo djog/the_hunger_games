dd3d5a459bcc47b869a736311c8334ff06b6feb7
RafayelGardishyan <rgardishyan@gmail.com>
2019-12-20 10:44:15 +0100

    Added main.py run function

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+from graphics.pygame_manager import Graphics
+
 if __name__ == "__main__":
     print("Hello SNEK")
+
+    p = Graphics((600, 600))
+
+    while p.Running:
+        p.parse_events()
+        p.draw()
+        p.close()
